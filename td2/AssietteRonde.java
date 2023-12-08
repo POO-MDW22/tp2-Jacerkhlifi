7bd83c5b6b9c3c48e19fba0413edc7c4401d10a3
Jacerkhlifi <147337815+Jacerkhlifi@users.noreply.github.com>
2023-12-08 13:20:27 +0100

    Update AssietteRonde.java

--- td2/AssietteRonde.java
+++ td2/AssietteRonde.java
@@ -7,9 +7,16 @@ public class AssietteRonde extends Assiette{
         public double getrayon(){
         return rayon;
         }
-    public double calculesurface(){
+        public double calculesurface(){
         return 3.14*getrayon()*getrayon();
         }
+    public String toString() {
+        return "L'année de fabrication est " + get_annneefabrication() + ". Le rayon est " + this.rayon;
+    }    public void setRrayon(double rayon) {
+        this.rayon = rayon;
+    }
+
+
 
 
 
