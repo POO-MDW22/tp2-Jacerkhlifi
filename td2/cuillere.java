e36fc0bee6f7fc21e5e4a151c25236446890193b
Jacerkhlifi <147337815+Jacerkhlifi@users.noreply.github.com>
2023-12-08 13:21:24 +0100

    Update cuillere.java

--- td2/cuillere.java
+++ td2/cuillere.java
@@ -3,9 +3,14 @@ public class cuillere extends Ustensile{
         public cuillere(int anneefabrication,double Longueur) {
             super(anneefabrication);
             this.Longueur = Longueur;
-        }
-            public double getLongueur() {
+        }public double getLongueur() {
                 return Longueur;
             }
 
-                }
+    public void setLongeur(double longeur) {
+        this.Longueur= longeur;
+    }
+    public String toString() {
+        return "lannee de fabrication est"+get_annneefabrication()+"la longuer est"+this.Longueur;
+    }
+}
